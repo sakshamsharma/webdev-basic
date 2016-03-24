38cc59f90ea1d9639946a08b071afbcd8450bf48
Saksham Sharma <saksham0808@gmail.com>
2016-03-24 12:01:31 +0530

    Add better query interface.

--- controllers/run-query.php
+++ controllers/run-query.php
@@ -1,28 +1,45 @@
 <?php
 include '../redirection.php';
 $db = new SQLite3('iitk');
-if (isset($_POST['query'])) {
-  $result = $db->query($_POST['query']);
-  echo "<table style='width:100%'>";
+$str = '';
+if ( $_POST['query'] != '') {
+  $str = $_POST['query'];
+} else {
+  $fields = $_POST['fields'];
+  $database = $_POST['database'] . $_POST['database2'] . $_POST['database3'];
+  $where = $_POST['where'];
+  $having = $_POST['having'];
 
-  // First print headers
-  $i = 0;
-  echo "<thead><tr class='heading'>";
-  for($i=0; $i < $result->numColumns(); $i++) {
-    echo "<td class='heading'><b>";
-    echo $result->columnName($i);
-    echo "</b></td>";
+  $str = sprintf("SELECT %s FROM %s", $fields, $database);
+  if ($where != '') {
+    $str = $str . " where " . $where;
   }
-  echo "</tr></thead>";
-
-  while($row = $result->fetchArray()) {
-    for($i=0; $i < $result->numColumns(); $i++) {
-      echo "<td>";
-      echo $row[$i];
-      echo "</td>";
-    }
-    echo "</tr>";
+  if ($having != '') {
+    $str = $str . " having " . $having;
   }
-  echo "</table>";
 }
+  echo $str;
+
+$result = $db->query($str);
+echo "<table style='width:100%'>";
+
+// First print headers
+$i = 0;
+echo "<thead><tr class='heading'>";
+for($i=0; $i < $result->numColumns(); $i++) {
+  echo "<td class='heading'><b>";
+  echo $result->columnName($i);
+  echo "</b></td>";
+}
+echo "</tr></thead>";
+
+while($row = $result->fetchArray()) {
+  for($i=0; $i < $result->numColumns(); $i++) {
+    echo "<td>";
+    echo $row[$i];
+    echo "</td>";
+  }
+  echo "</tr>";
+}
+echo "</table>";
 ?>
--- views/query.php
+++ views/query.php
@@ -7,9 +7,40 @@
 <body>
   <a href="../controllers/logout.php">Logout</a>
   <div class=box>
-    Search the iitk database
+    Search the iitk database (either type query, or use inputs)
     <form method ="post" action="./query.php">
       <input class="inp-box" name="query" placeholder="Query" autocomplete="off"/>
+
+      <label>Database</label>
+      <select name="database">
+        <option value="students">Students</option>
+        <option value="profs">Profs</option>
+        <option value="courses">Courses</option>
+        <option value="enrollment">Enrollment</option>
+      </select>
+
+      <label>Database 2 (Optional)</label>
+      <select name="database2">
+        <option value=""></option>
+        <option value=",students">Students</option>
+        <option value=",profs">Profs</option>
+        <option value=",courses">Courses</option>
+        <option value=",enrollment">Enrollment</option>
+      </select>
+
+      <label>Database 3 (Optional)</label>
+      <select name="database3">
+        <option value=""></option>
+        <option value=",students">Students</option>
+        <option value=",profs">Profs</option>
+        <option value=",courses">Courses</option>
+        <option value=",enrollment">Enrollment</option>
+      </select>
+
+      <input class="inp-box" name="fields" placeholder="Enter fields. eg: Roll, Name" autocomplete="off"/>
+      <input class="inp-box" name="where" placeholder="Enter where clause" autocomplete="off"/>
+      <input class="inp-box" name="having" placeholder="Enter having clause" autocomplete="off"/>
+
       <p>
       <input class="button buttonBlue" name="submit" type="Submit" value="Query"/>
       <input class="button" name="reset" type="reset" value="Clear Form">
